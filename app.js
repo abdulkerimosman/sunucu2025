7e54bd66ad5e49ba9471e1c65ab7879b111efbbb
Abdulkerim <abdulkerimissa1@gmail.com>
2024-11-16 09:18:15 +0300

    json

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT 
 app.get("/",(req,res)=>{ // or post get for when something is wanted from us, post is when we want something   ///// '/' this shows the first page i.e. index.html
   res.json({
-    message:'Welcome!' // the 
+    message:'Welcome!' // the m
   })
 })
 app.listen(port,()=>{
