3439d75335ac750bb9e7588dcb5c09615170bb1e
Abdulkerim <abdulkerimissa1@gmail.com>
2024-11-16 09:17:17 +0300

    main ones

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express") 
+require('dotenv/config')
+const db_connect=require("./db/mysql_connect")
+const app = express() 
+const port = process.env.PORT 
+app.get("/",(req,res)=>{ // or post get for when something is wanted from us, post is when we want something   ///// '/' this shows the first page i.e. index.html
+  res.json({
+    message:'Welcome!' // the 
+  })
+})
+app.listen(port,()=>{
+  console.log(`Sunucu ${port} is working!`)
+})
